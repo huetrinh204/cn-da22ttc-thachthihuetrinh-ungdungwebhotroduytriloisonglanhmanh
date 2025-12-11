f7e4126943ab16c95831c8bd025895b127a8de90
huetrinh204 <trinhfokko@gmail.com>
2025-12-11 18:52:04 +0700

    fixing chart for habit- edit web clean

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ try {
     // Chuyển charset sang utf8mb4 để hỗ trợ emoji
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
     // Đảm bảo kết nối sử dụng utf8mb4
     $pdo->exec("SET NAMES 'utf8mb4'");
 } catch (PDOException $e) {
